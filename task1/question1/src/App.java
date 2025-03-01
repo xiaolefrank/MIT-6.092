643c461f3fe8f4849e9ff3ba000c83ca90ce27d7
xiaolefrank <103012408+xiaolefrank@users.noreply.github.com>
2025-03-01 19:20:10 +0800

    Add files via upload

---
+++ task1/question1/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String foo="IAP6.092";
+        System.out.println(foo);
+        foo="something else";
+        System.out.println(foo);
+        boolean bar=false;
+        System.out.println(bar);
+    }
+}
---
+++ task1/question1/src/DoMath2.java
@@ -0,0 +1,13 @@
+public class DoMath2 {
+    public static void main(String[] args) {
+        double score=1.0+2.0*3.0;
+        System.out.println(score);
+        double copy=score;
+        copy=score/2.0;
+        System.out.println(copy);
+        System.out.println(score);
+        String text="hello"+" world";
+        text=text+" number "+5;
+        System.out.println(text);
+    }
+}
